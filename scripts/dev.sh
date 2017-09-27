10391f9277f8dc9cbcf93301a8431aff9a81e081
Taylor Harwood <htaylor127@gmail.com>
2017-09-27 11:21:39 -0700

    Upgrades to React 16

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,10 +3,10 @@
 cwd=$(pwd)
 project_name=zen-camp-client
 
-docker build -t project_name .
+docker build -t ${project_name} .
 
 docker run \
     -v ${cwd}/src:/usr/src/app/src \
     -p 8081:8081 \
     --env-file .env \
-    -it project_name /bin/bash
+    -it ${project_name} /bin/bash
